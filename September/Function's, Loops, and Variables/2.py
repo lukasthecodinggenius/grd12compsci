fcf74011f3215ddea714f50b2df91ed68742ebee
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-09-12 13:25:11 -0400

    Update 2.py

--- September/Function's, Loops, and Variables/2.py
+++ September/Function's, Loops, and Variables/2.py
@@ -1,12 +1,13 @@
 def main():
+    #take input
     num = int(input('input number: '))
     final = factorial(int(num))
     print(final)
 
 def factorial(num):
     total = 1
-    for i in range(1, num+1):
-        total *= i
-    return total
+    for i in range(1, num+1): #for loop will repeat num+1 amount of times
+        total *= i #total = total * i
+    return total #return the factorial of the number
 
-main()
+main() #call main
