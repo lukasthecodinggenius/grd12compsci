65622e4c36287f45df19a1734f63615a77d79073
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-09-12 13:22:26 -0400

    Update 1.py

--- September/Function's, Loops, and Variables/1.py
+++ September/Function's, Loops, and Variables/1.py
@@ -1,13 +1,16 @@
 def main():
+    #take input
     number = input('Input Number: ')
     type = evenorodd(number)
 
+    #print based on result
     if type == True:
         print('even')
     else:
         print('odd')
 
 def evenorodd(num):
+    #if the remainer when divided by 2 is 0, then the number is even
     if int(num) % 2 == 0:
         return True
     else:
