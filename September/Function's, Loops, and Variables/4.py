58ba6ac7f45f3b91287e7e1b6c1e32a9605ff514
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-09-12 13:27:05 -0400

    Update 4.py

--- September/Function's, Loops, and Variables/4.py
+++ September/Function's, Loops, and Variables/4.py
@@ -1,12 +1,12 @@
 def main():
-    for i in range(51):
-        if i % 3 == 0 and i % 5 == 0:
+    for i in range(51): #this for loop will repeat 51 times
+        if i % 3 == 0 and i % 5 == 0: #first, check if the number is a factor of 3 and 5
             print('FizzBuzz')
-        elif i % 3 == 0:
+        elif i % 3 == 0: #then check if its a factor of 3 
             print('Fizz')
-        elif i % 5 == 0:
+        elif i % 5 == 0: #then check if its a factor of 5
             print('Buzz')
         else:
-            print(i)
+            print(i) #if it doesn't meet any of the requirements, just print I
 
 main()
