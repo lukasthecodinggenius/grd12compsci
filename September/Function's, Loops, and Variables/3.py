64364adb1ac926b3f587ad0635dcbafd7d4dd91d
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-09-12 13:26:12 -0400

    Update 3.py

--- September/Function's, Loops, and Variables/3.py
+++ September/Function's, Loops, and Variables/3.py
@@ -1,9 +1,10 @@
 def palindrome(w):
+    #return True or False based on the reversed value of the string
     return w == w[::-1]
 
-check = input('enter word: ')
+check = input('enter word: ') #take input
 
-if palindrome(check) == True:
+if palindrome(check) == True: #output based on the result of calling palindrome
     print('yes')
 else:
     print('no')
