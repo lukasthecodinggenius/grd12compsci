15f77feb9079d35855be787e04c72500ece7343d
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-08 13:54:08 -0400

    Create 2.py

---
+++ September/2.py
@@ -0,0 +1,6 @@
+def main():
+    
+    length, width = int(input('Enter Length: ')), int(input('Enter Width: '))
+    print(f'Area = {length * width}, Perimeter: {length + length + width + width}')
+
+main()
