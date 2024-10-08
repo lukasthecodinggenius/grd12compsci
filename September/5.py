4a8734e66b9b64454516f404965cf6dd24505eef
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-08 19:24:24 -0400

    Create 5.py

---
+++ September/5.py
@@ -0,0 +1,9 @@
+def main():
+    
+    
+    first, last, age = input('Enter First Name: '), input('Enter Last Name: '), input('Enter Age: ')
+    
+    print(f'Hi {first} {last}! Let me guess, you are {age} years old?!!?!?!?!?')
+    
+    
+main()
