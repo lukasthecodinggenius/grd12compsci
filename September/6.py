20e7513137e0b25da8a8388c0428d8510c3f8ad8
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-08 20:13:03 -0400

    Create 6.py

---
+++ September/6.py
@@ -0,0 +1,12 @@
+def main():
+    
+    
+    num1, num2 = int(input('Number 1: ')), int(input('Number 2: '))
+    print(f'sum: {num1 + num2}')
+    print(f'difference: {num1-num2}')
+    print(f'product: {num1*num2}')
+    quotient = num1 / num2
+    print(f'quotient: {quotient:.02f}')
+    
+    
+main()
