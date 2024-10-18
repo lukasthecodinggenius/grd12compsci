3c0d13e8dba4d1569977811f7e5287991a4993b7
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-17 23:34:19 -0400

    Create test_car.py

---
+++ October/Practical Lab/test_car.py
@@ -0,0 +1,9 @@
+from car import Car
+
+my_car = Car('Honda', 'Civic')
+
+my_car.display_state()  
+my_car.start()          
+my_car.display_state()  
+my_car.stop()           
+my_car.display_state()  
