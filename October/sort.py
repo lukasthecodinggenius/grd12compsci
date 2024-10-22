c93b1142362b950395cbe959e96abdde3c206763
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-22 09:33:52 -0400

    Create sort.py

---
+++ October/sort.py
@@ -0,0 +1,11 @@
+def main():
+    
+    unsorted = [4, 5, 1, 94, 12039, 43, 23, 0, 3]
+    print(sort_list(unsorted))
+    
+def sort_list(list):
+    
+    return sorted(list)
+    
+if __name__ == '__main__':
+    main()
