4d011c21ce0f202c2adfc844807aee7dfea2708f
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-22 09:49:39 -0400

    Create test4.py

---
+++ October/test4.py
@@ -0,0 +1,11 @@
+from leap import leap_year
+
+def main():
+    
+    print(leap_year(1997))
+    print(leap_year(400))
+    print(leap_year(12358400))
+    print(leap_year(804))
+
+if __name__ == '__main__':
+    main()
