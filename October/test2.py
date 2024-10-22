3dec7ca6d7f37fe182bb1cc016a0012710c2bc16
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-10-22 09:34:11 -0400

    Create test2.py

---
+++ October/test2.py
@@ -0,0 +1,9 @@
+from sort import sort_list
+
+def main():
+    
+    unsorted = [10,23,2134,1235,57,65,745,34,235,78,7,2,342,36,7]
+    print(sort_list(unsorted))
+
+if __name__ == '__main__':
+    main()
