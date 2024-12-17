4ee0494e113d5245a57a4bf3c714a3dc582d6963
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-12-16 21:43:04 -0500

    Update 3.py

--- December/3.py
+++ December/3.py
@@ -8,7 +8,7 @@ def main():
     
 def validate(password):
     
-    if len(password) >= 8 and re.search('[A-Z]+', password) and re.search('[0-9]+', password):
+    if len(password) >= 8 and re.search('[A-Z]+', password) and re.search('[0-9]+', password): #use regular expressions to check for more than one instance of capital letters and digits
         return True
     else:
         return False
