8caea15098e4022f2bbb25218c3ad86a650b5616
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-12-16 21:42:37 -0500

    Update 4.py

--- December/4.py
+++ December/4.py
@@ -8,7 +8,7 @@ def main():
     
 def palindrome(string):
     
-    return string == string[::-1]
+    return string == string[::-1] #check if the string is the same when reversed, return True if it is, False if it isnt
         
 
 main()
